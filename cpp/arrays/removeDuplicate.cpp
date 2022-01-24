206699b422fa3f7c0def8068fe0d7dfd00f928b7
bharath2612 <bharathchippa49@gmail.com>
2022-01-24 16:54:06 +0530

    nothing big

--- cpp/arrays/removeDuplicate.cpp
+++ cpp/arrays/removeDuplicate.cpp
@@ -8,7 +8,7 @@ int remDups(int arr[], int n)
 {
     int res = 1;
 
-    for (int i = 1; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
         if (arr[res - 1] != arr[i])
         {
--- cpp/arrays/trappingRain.cpp
+++ cpp/arrays/trappingRain.cpp
@@ -2,32 +2,32 @@
 using namespace std;
 
 // O(n) implementation
-int trappingRain(int arr[], int size)
+int trappingRain(int height[], int size)
 {
     int res = 0;
     int lmax[size], rmax[size];
-    lmax[0] = arr[0];
+    lmax[0] = height[0];
     for (int i = 1; i < size; i++)
     {
-        lmax[i] = max(arr[i], lmax[i - 1]);
+        lmax[i] = max(height[i], lmax[i - 1]);
     }
-    rmax[size - 1] = arr[size - 1];
+    rmax[size - 1] = height[size - 1];
     for (int i = size - 2; i >= 0; i--)
     {
-        rmax[i] = max(arr[i], rmax[i + 1]);
+        rmax[i] = max(height[i], rmax[i + 1]);
     }
     for (int i = 0; i < size; i++)
     {
-        res = res + (min(lmax[i], rmax[i]) - arr[i]);
+        res = res + (min(lmax[i], rmax[i]) - height[i]);
     }
     return res;
 }
 
 int main()
 {
-    int arr[] = {3, 0, 1, 2, 3, 4, 0, 5};
+    int height[] = {3, 0, 1, 2, 3, 4, 0, 5};
     cout
-        << "maximum trapping section is " << trappingRain(arr, 8)
+        << "maximum trapping section is " << trappingRain(height, 8)
         << endl;
     return 0;
 }
